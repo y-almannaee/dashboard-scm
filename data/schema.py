0d7d2a6a48449822f55ab9a0374090956b441bc6
Yaseen <example@example.org>
2026-04-09 21:56:32 +0400

    Placeholder Streamlit dashboard

--- data/schema.py
+++ data/schema.py
@@ -3,14 +3,18 @@ from typing import Any, Dict
 from datetime import datetime, timedelta
 
 # universal data model
-class UDM(dataclasses):
+@dataclasses.dataclass
+class UDM:
 	SKU: str
 	Grade: str
 	Origin: str
 	LeadTime: timedelta
-	BOM: Dict[BOM, Any]
+	BOM: Dict[str, Any]
 
-	def __init__(self):
+	def __init__(self, **kwargs):
+		# Set attributes from kwargs
+		for key, value in kwargs.items():
+			setattr(self, key, value)
 		pass
 
 class BOM:
